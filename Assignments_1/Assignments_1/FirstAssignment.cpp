28699ac2851a4503d3003cd4cffa98ce36ca2dbf
LuisG94 <44033039+LuisG94@users.noreply.github.com>
2019-01-21 21:32:40 -0600

    a

--- Assignments_1/Assignments_1/FirstAssignment.cpp
+++ Assignments_1/Assignments_1/FirstAssignment.cpp
@@ -1,5 +1,6 @@
+#pragma warning(disable:4996)
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <math.h>
 #include <iostream>
 
@@ -13,12 +14,31 @@ int main()
 	for (int i = 0; i < 6; i++)
 	{
 		fscanf(f, "%f", &values[i]);
-		printf("%f", values[i]);
+		printf("%f\n", values[i]);
+		sum += values[i];
+		total++;
+	}
+	fclose(f);
+
+	average = sum / total;
+
+	/*for (int i = 0; i < total; i++)
+	{
+		variance += pow(values[i] - average, 2);
 	}
 
+	variance = variance / total;
+	stdDev = sqrt(variance);*/
 
 
+	printf("The Average of the values = %f\n", average);
+	printf("The Standard Deviation of the values = %f\n", average);
+	
 
 
+	system("pause");
+
+
+	return 0;
 
 }
