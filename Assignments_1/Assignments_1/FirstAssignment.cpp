ccc8eef62375c8de770256ef017c16b49097196a
LuisG94 <44033039+LuisG94@users.noreply.github.com>
2019-01-21 21:57:21 -0600

    aaaa

--- Assignments_1/Assignments_1/FirstAssignment.cpp
+++ Assignments_1/Assignments_1/FirstAssignment.cpp
@@ -7,32 +7,32 @@
 int main()
 {
 	int total = 0;
-	float sum = 0.0, variance = 0.0, average, stdDev, values[] = { 0.0 };
+	float sum = 0.0, variance = 0.0, average = 0.0, stdDev, values[6] = { 0.0 };
 
 	FILE *f = fopen("numbers.txt", "r");
 
 	for (int i = 0; i < 6; i++)
 	{
 		fscanf(f, "%f", &values[i]);
-		printf("%f\n", values[i]);
-		sum += values[i];
+		printf("%.3f\n", values[i]);
+		sum = sum + values[i];
 		total++;
 	}
 	fclose(f);
 
 	average = sum / total;
 
-	/*for (int i = 0; i < total; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		variance += pow(values[i] - average, 2);
 	}
 
 	variance = variance / total;
-	stdDev = sqrt(variance);*/
+	stdDev = sqrt(variance);
 
 
 	printf("The Average of the values = %f\n", average);
-	printf("The Standard Deviation of the values = %f\n", average);
+	printf("The Standard Deviation of the values = %f\n", stdDev);
 	
 
 
