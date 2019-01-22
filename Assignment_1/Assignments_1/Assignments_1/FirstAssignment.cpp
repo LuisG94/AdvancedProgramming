c46320e2df749374d55315e85952b8a24142e324
LuisG94 <44033039+LuisG94@users.noreply.github.com>
2019-01-21 20:57:33 -0600

    aaa

---
+++ Assignment_1/Assignments_1/Assignments_1/FirstAssignment.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+
+int main()
+{
+	int total = 0;
+	float sum = 0.0, variance = 0.0, average, stdDev, values[] = { 0.0 };
+
+	FILE *f = fopen("numbers.txt", "r");
+
+
+
+
+
+
+
+
+}
